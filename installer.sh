94e8dcdfd62ba8b79a5549bfaf777210c5a9fb45
uriel <urielcruz2099@outlpook.com>
2021-12-27 20:52:39 -0600

    Creacion de una script de instalacion

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 apt-get update
-apt-get install build-essential checkinstall
-apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+apt-get install -y noconfirm build-essential checkinstall
+apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev \
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 cd /opt
 wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
 tar xzf Python-3.8.12.tgz
 cd Python-3.8.12
-sudo ./configure --enable-optimizations
-sudo make altinstall
+./configure --enable-optimizations
+make altinstall
